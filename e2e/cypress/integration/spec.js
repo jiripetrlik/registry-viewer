dfa1ec59c4a5c4f4b96f4f13cbc90738dd47ed5d
Jiri Petrlik <jiripetrlik@gmail.com>
2021-12-26 11:56:57 +0100

    Implement e2e tests

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,3 +1,64 @@
-it("connect to app", () => {
-    cy.visit("/") 
+it("Show app", () => {
+    cy.visit("/")
+    cy.title().should('eq', 'Registry viewer')
+})
+
+it("Connect to a Docker registry", () => {
+    cy.visit("/")
+
+    cy.title().should('eq', 'Registry viewer')
+    cy.contains("Connect to Docker registry")
+
+    cy.get("#connect_form")
+    cy.get("#url").type("http://registry:5000")
+    cy.get("#insecure1")
+    cy.get("#use_authentication_button")
+    cy.get("input[type=submit]").click()
+
+    cy.contains("Connected to registry")
+    cy.contains("Test connection").click()
+    cy.contains("Successfully connected!")
+})
+
+it("Catalog test", () => {
+    cy.visit("/")
+
+    cy.title().should('eq', 'Registry viewer')
+    cy.contains("Connect to Docker registry")
+
+    cy.get("#connect_form")
+    cy.get("#url").type("http://registry:5000")
+    cy.get("#insecure1")
+    cy.get("#use_authentication_button")
+    cy.get("input[type=submit]").click()
+
+    cy.contains("Catalog").click()
+    cy.contains("alpine")
+    cy.contains("busybox")
+    cy.contains("registry").parent().contains("View tags").click()
+    cy.contains("latest").parent().contains("Manifest").click()
+
+    cy.contains("Manifest detail")
+    cy.contains("Manifest")
+    cy.contains("Manifest config")
+    cy.contains("Layers")
+})
+
+it("Disconnect test", () => {
+    cy.visit("/")
+
+    cy.title().should('eq', 'Registry viewer')
+    cy.contains("Connect to Docker registry")
+
+    cy.get("#connect_form")
+    cy.get("#url").type("http://registry:5000")
+    cy.get("#insecure1")
+    cy.get("#use_authentication_button")
+    cy.get("input[type=submit]").click()
+
+    cy.contains("Connected to registry")
+    cy.contains("Disconnect").click()
+    cy.get("div[role=dialog]").get("div[class=ui-dialog-buttonset]").contains("Disconnect").click()
+
+    cy.get("#connect_form")
 })
